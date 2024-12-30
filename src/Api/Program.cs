debadf9d47105ad479b5f00f05312b9ac663d3dd
danilo0609 <daniusugaj@gmail.com>
2024-12-29 20:27:21 -0500

    Adding basic configuration for application startup and connection to other services

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,9 +1,70 @@
+using Carter;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using SavePlan.API;
+using SavePlan.API.Infrastructure;
+using SavePlan.API.Middlewares;
+using Serilog;
+using System.Collections;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+var jsonConfig = new ConfigurationBuilder()
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+    .Build();
+
+// Get all environment variables
+var environmentalVariables = Environment.GetEnvironmentVariables();
+
+// Replace placeholders with environment variables
+foreach (var child in jsonConfig.AsEnumerable())
+{
+    if (!string.IsNullOrEmpty(child.Value) && child.Value.Contains("${"))
+    {
+        var updatedValue = child.Value;
+
+        foreach (DictionaryEntry env in environmentalVariables)
+        {
+            string placeholder = $"${{{env.Key}}}";
+            updatedValue = updatedValue.Replace(placeholder, env.Value?.ToString());
+        }
+
+        // Update configuration
+        builder.Configuration[child.Key] = updatedValue;
+    }
+}
+builder.Host.UseSerilog((context, loggerConfig) =>
+{
+    loggerConfig.ReadFrom.Configuration(context.Configuration);
+});
+
+builder.Services.AddHttpContextAccessor();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
+
+builder.Services.AddProblemDetails();
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddTransient<RequestLogContextMiddleware>();
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(o =>
+    {
+        o.RequireHttpsMetadata = false;
+        o.Audience = builder.Configuration["Authentication:Audience"];
+        o.MetadataAddress = builder.Configuration["Authentication:MetadataAddress"]!;
+
+        o.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidIssuer = builder.Configuration["Authentication:ValidIssuer"],
+        };
+    });
+
+
+var serilog = builder.Configuration.GetSection("Serilog");
+var database = builder.Configuration.GetConnectionString("Database");
 
 var app = builder.Build();
 
@@ -14,31 +75,20 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UsePathBase("/api");
+
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseMiddleware<RequestLogContextMiddleware>();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapCarter();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.UseSerilogRequestLogging();
+
+app.UseExceptionHandler();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
