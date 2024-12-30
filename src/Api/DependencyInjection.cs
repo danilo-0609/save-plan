012ec8ac97cb02297e8efb65068afaaf280c3a9d
danilo0609 <daniusugaj@gmail.com>
2024-12-29 20:28:56 -0500

    Injecting services created for features

---
+++ src/Api/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using Carter;
+using FluentValidation;
+using MediatR;
+using SavePlan.API.Behaviors;
+using SavePlan.API.Common;
+
+namespace SavePlan.API;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var assembly = typeof(Program).Assembly;
+
+
+        services.AddCarter();
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssemblyContaining<Program>();
+
+            config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+            config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            config.AddOpenBehavior(typeof(UnitOfWorkBehavior<,>));
+        });
+
+        services.AddValidatorsFromAssembly(assembly, includeInternalTypes: true);
+
+        services.AddScoped<IExecutionContextAccessor, ExecutionContextAccessor>();
+
+        return services;
+    }
+}
