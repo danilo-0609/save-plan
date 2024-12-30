5c5c10820442d0c6022b57e1424e8403f1e5e915
danilo0609 <daniusugaj@gmail.com>
2024-12-29 20:27:54 -0500

    Adding http middlewares for error handling and logging

---
+++ src/Api/Middlewares/GlobalExceptionHandler.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http.Connections.Features;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace SavePlan.API.Middlewares;
+
+public class GlobalExceptionHandler : IExceptionHandler
+{
+    private readonly ILogger<GlobalExceptionHandler> _logger;
+    private readonly IProblemDetailsService _problemDetailsService;
+
+    public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger, IProblemDetailsService problemDetailsService)
+    {
+        _logger = logger;
+        _problemDetailsService = problemDetailsService;
+    }
+
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext, 
+        Exception exception, 
+        CancellationToken cancellationToken)
+    {
+        _logger.LogError(exception, "Exception occurred: {Message}", exception.Message);
+
+        Activity? activity = httpContext.Features.Get<IHttpActivityFeature>()?.Activity;
+        return await _problemDetailsService.TryWriteAsync(new ProblemDetailsContext
+        {
+            HttpContext = httpContext,
+            Exception = exception,
+            ProblemDetails = new ProblemDetails()
+            {
+                Status = StatusCodes.Status500InternalServerError,
+                Title = "Server error",
+                Type = exception.GetType().Name,
+                Detail = exception.Message,
+                Instance = $"{httpContext.Request.Method} {httpContext.Request.Path}",
+                Extensions = new Dictionary<string, object?>
+                {
+                    { "requestId", httpContext.TraceIdentifier },
+                    { "traceId", activity?.TraceId }
+                }
+            }
+        });
+    }
+}
---
+++ src/Api/Middlewares/RequestLogContextMiddleware.cs
@@ -0,0 +1,15 @@
+﻿
+using Serilog.Context;
+
+namespace SavePlan.API.Middlewares;
+
+public sealed class RequestLogContextMiddleware : IMiddleware
+{
+    public Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        using (LogContext.PushProperty("CorrelationId", context.TraceIdentifier))
+        {
+            return next(context);
+        }
+    }
+}
